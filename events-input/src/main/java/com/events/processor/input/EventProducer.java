baf261928eefecf8cd56d8c243678366c8ff3e92
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2018-01-31 10:26:27 +0100

    sample files for using apache spark as consumer and kafka as producer

--- events-input/src/main/java/com/events/processor/input/EventProducer.java
+++ events-input/src/main/java/com/events/processor/input/EventProducer.java
@@ -1,33 +1,29 @@
 package com.events.processor.input;
 
-import com.events.processor.input.domain.Event;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 public class EventProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "com.events.processor.serializer.EventSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, Event> eventProducer = new KafkaProducer<>(props);
-        Map<String, Integer> experimentMap = new HashMap<>();
-        experimentMap.put("experiment1", 1);
-        experimentMap.put("experiment2", 2);
-        Event event = new Event("1", "a", experimentMap);
-        eventProducer.send(new ProducerRecord<>("TrackingEvents", event.getEventId(), event));
+        Producer<String, String> eventProducer = new KafkaProducer<>(props);
+        for (int i = 0; i < 1000; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("test", "value-" + i);
+            eventProducer.send(record);
+            Thread.sleep(50);
+        }
         eventProducer.close();
-
     }
 }
