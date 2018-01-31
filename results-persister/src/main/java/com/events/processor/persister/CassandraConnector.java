9adba03114fee5dec39202c4db21857e2a6e49ff
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2018-01-31 11:54:08 +0100

    inet socket address.

--- results-persister/src/main/java/com/events/processor/persister/CassandraConnector.java
+++ results-persister/src/main/java/com/events/processor/persister/CassandraConnector.java
@@ -3,6 +3,9 @@ package com.events.processor.persister;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
 
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+
 public class CassandraConnector {
 
     private Cluster cluster;
@@ -11,7 +14,8 @@ public class CassandraConnector {
 
     public Session connect(String node, Integer port) {
         cluster = Cluster.builder()
-                .addContactPoint(node).build();
+                .addContactPointsWithPorts(Arrays.asList(new InetSocketAddress(node, port)))
+                .build();
         session = cluster.connect();
         return session;
     }
